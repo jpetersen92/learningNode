fa90f26a9e11249b43a40fb12eb821033678d26c
Josh Petersen <jdpetersen92@gmail.com>
2022-04-07 10:53:13 -0700

    learning about node modules

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-require('./logger')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World');
+        res.end;
+    }
+
+    
+
+});
+
+
+
+server.listen(3000);
+
+console.log('Listening on port 3000...');
--- logger.js
+++ logger.js
@@ -1,8 +1,16 @@
+const EventEmitter =  require('events');
+
 const url = 'http://mylogger.io/log'
 
-const log = (message) => {
-    // Send an HTTP request
-    console.log(message)
+class Logger extends EventEmitter {
+
+    log(message) {
+        // Send an HTTP request
+        console.log(message);
+        // Raise Event
+        this.emit('messageLogged', { id: 1, url: 'http://'});
+    }
 }
 
-module.exports.log =  log
+
+module.exports = Logger;
